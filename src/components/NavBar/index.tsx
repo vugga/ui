d1325545c1e087f103ca9dab8d255689b8af59c0
Ceddy Muhoza <ceddymuhoza@gmail.com>
2019-03-27 10:36:26 -0400

    Drawer and NavBar

---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import NavbarStyle from './navbar.style';
+
+interface INavBar {
+  className?: string;
+  /** Used to render menu, logo, button or any component that
+   * you want to show in navbar. */
+  children?: Element;
+  navbarStyle?: string;
+  width?: string | number;
+  height?: string | number;
+  space?: string | number;
+  borderRadius?: string | number;
+  boxShadow?: string;
+  color?: string;
+  display?: string;
+  alignItems?: string;
+  justifyContent?: string;
+  flexDirection?: string;
+  flexWrap?: string;
+}
+
+const Navbar = (navProps: INavBar) => {
+  const { className, children, navbarStyle, ...props } = navProps;
+  
+  // Add all classs to an array
+  const addAllClasses = ['reusecore__navbar'];
+
+  // className prop checking
+  if (className) {
+    addAllClasses.push(className);
+  }
+
+  return (
+    <NavbarStyle className={addAllClasses.join(' ')} {...props}>
+      {children}
+    </NavbarStyle>
+  );
+};
+
+export default Navbar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
 export * from './components/BottomNav';
 export * from './components/Smart';
 export * from './components/Smart/x.css';
+export * from './components/Drawer';
+export * from './components/NavBar';
